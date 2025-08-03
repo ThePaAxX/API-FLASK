e26599ec30f0b835a9529d84eb09deaea52e5749
Juan Arellano <jarellano@Juans-MacBook-Pro.local>
2025-08-02 20:10:39 -0400

    commit init

---
+++ app.py
@@ -0,0 +1,121 @@
+import uuid
+from flask import Flask, request
+from flask_smorest import abort
+from db import items, stores
+
+app = Flask(__name__)
+
+@app.get('/stores')
+def get_stores():
+    return {'stores': list(stores.values())}
+
+# @app.get('/stores')
+# def get_stores():
+#     return {'stores': stores}
+
+@app.get('/stores/<string:store_id>')
+def get_store(store_id):
+    try:
+        return stores[store_id], 200
+    except KeyError:
+        abort(404, message='Store not found.')
+
+@app.post('/stores')
+def create_stores():
+    store_data  = request.get_json()
+    if 'name' not in store_data:
+        abort(400, message='Bad request. Ensure "name" is included in the JSON payload.')
+    
+    for store in stores.values():
+        if store_data['name'] == store['name']:
+            abort(400, message='Store with this name already exists.')
+
+    store_id = uuid.uuid4().hex
+    store = { **store_data, 'id': store_id, }
+    stores[store_id] = store
+    return store, 201
+
+
+
+
+@app.post('/item')
+def create_item():
+    item_data = request.get_json()
+    if (
+        'store_id' not in item_data or
+        'name' not in item_data or
+        'price' not in item_data
+    ):
+        abort(
+            400, 
+            message='Bad request. Ensure store_id, name, and price are provided.'
+        )
+    
+    for key in item_data.keys():
+        if key not in ['store_id', 'name', 'price']:
+            abort(
+                400, 
+                message=f'Invalid field: {key}')
+    
+    for item in items.values():
+        if (
+            item_data['name'] == item['name'] and
+            item_data['store_id'] == item['store_id']
+        ):
+            abort(
+                400,
+                message='Item already exists in this store.'
+            )
+
+    if item_data['store_id'] not in stores:
+        abort(404, message='Store not found.')
+
+    item_id = uuid.uuid4().hex
+    item = { **item_data, 'id': item_id }
+    items[item_id] = item
+
+    return item, 201
+
+@app.get('/item')
+def get_all_item():
+    return {'items': list(items.values())}, 200
+
+@app.get('/item/<string:item_id>')
+def get_item(item_id):
+    try:
+        return items[item_id], 200
+    except KeyError:
+        abort(404, message='Item not found.')
+
+@app.delete('/item/<string:item_id>')
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {'message': 'Item deleted successfully.'}, 200
+    except KeyError:
+        abort(404, message='Item not found.')
+
+@app.put('/item/<string:item_id>')
+def update_item(item_id):
+    item_data = request.get_json()
+
+    if 'name' not in item_data and 'price' not in item_data:
+        abort(
+            400,
+            message='Bad request. Ensure at least one of "name" or "price" is provided.'
+        )
+
+    for key in item_data.keys():
+        if key not in ['name', 'price']:
+            abort(
+                400,
+                message=f'Invalid field: {key}'
+            )
+    
+    try:
+        item = items[item_id]
+    except KeyError:
+        abort(404, message='Item not found.')
+
+    item.update(item_data)
+    return item, 200
